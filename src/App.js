2083f52233c716e47c06ba23513744c2b4d0b203
Ricram2 <ricram2@gmail.com>
2022-04-13 17:40:37 -0400

    Fixed Graphical Styles as per design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import PropTypes from "prop-types";
 import './App.css';
-//import MiniCard from './components/miniCard/miniCard'
 import { ReactComponent as RobbieLogo } from './Robbie.svg'
-import moment from "moment"
-//import { ReactComponent as BackgorundMask } from './BackgorundMask.svg'
+import moment from "moment";
 
 moment().format('MMMM Do YYYY, h:mm:ss a');
 function App({
@@ -26,34 +24,82 @@ function App({
 
   return (
     <div className="App">
-      <div className="logo">
-        <RobbieLogo></RobbieLogo>
+      <div className="letter-head">
+        <div className="logo">
+          <RobbieLogo></RobbieLogo>
+        </div>
+        <div className="claim-info">
+          <div className="blue-header">{headline}</div>
+          <div className="claim-header">Claim #: {claimNumber}</div>
+          <div className="subtitle"><span className="genDato">{address.split(',')[0]}</span></div>
+          <div className="subtitle"><span className="genDato">{postalCode}</span></div>
+        </div>
       </div>
-      <div>
-        <div className="generalData">{headline}</div>
-        <div className="generalData">Claim #: <span className="genDato">{claimNumber}</span></div>
-        <div className="generalData"><span className="genDato">{address.split(',')[0]}</span></div>
-        <div className="generalData"><span className="genDato">{postalCode}</span></div>
-      </div>
-      <div className="generalData">SUMMARY</div>
+
+
+      <div className="generalData"></div>
+      <div className="table-title blue-header">SUMMARY</div>
       <table width="100%">
-        <thead align="left"><tr><th>CLAIM SETUP</th><th>TIME ESTIMATION</th><th>ENERGY USED</th></tr></thead>
+
+        <thead align="left"><tr>
+          <th className="setup">CLAIM SETUP</th>
+          <th className="time">TIME ESTIMATION</th>
+          <th className="energy">ENERGY USED</th></tr>
+        </thead>
+
         <tbody align="left">
-          <tr><td>{totalAreas} Areas</td><td>{totaldryingtime - demolitiontime} Drying Time</td><td>CAD$ {totalEnergyCostJob}</td></tr>
-          <tr><td>{totalCountDh} Dehumidifier - {totalCountBl} Blower</td><td>Pre-drying: {demolitiontime} - Total: {totaldryingtime}</td><td>{totalEnergyJob} Kwh</td></tr>
+
+          <tr>
+            <td>
+              <div>
+                <div>{totalAreas} Areas</div>
+                <div className="subtitle">{totalCountDh} Dehumidifier - {totalCountBl} Blower </div>
+              </div>
+            </td>
+
+            <td>
+              <div>{totaldryingtime - demolitiontime}h Drying</div>
+              <div className="subtitle">Pre-drying: {demolitiontime}h - Total: {totaldryingtime}h</div>
+            </td>
+
+            <td>
+              <div>CAD${totalEnergyCostJob}</div>
+              <div className="subtitle">{totalEnergyJob}kWh</div>
+            </td>
+          </tr>
         </tbody>
       </table>
-      <div className="generalData">BREAKDOWN</div>
+
+      <div className="generalData"></div>
+      <div className="table-title blue-header">BREAKDOWN</div>
       <table width="100%">
-        <thead align="left"><tr><th>AREA SETUP</th><th>TIME BREAKDOWN</th><th>ENERGY USED</th></tr></thead>
+
+        <thead align="left"><tr>
+          <th className="setup">AREA SETUP</th>
+          <th className="time">TIME BREAKDOWN</th>
+          <th className="energy">ENERGY USED</th></tr>
+        </thead>
+
         <tbody align="left">
           {areas.map((area, index) => (
             <>
               <tr>
-                <td>{area.name}</td><td>{(totalLapsedTime - area.dryingTime).toFixed(2)} Drying Time</td><td>CAD$ {(area.totalEnergy * 0.17).toFixed(2)}</td>
-              </tr>
-              <tr>
-                <td>{area.dh} Dehumidifier - {area.bl} Blower</td><td>Pre-drying: {area.dryingTime} - Total: {totalLapsedTime}</td><td>{area.totalEnergy} Kwh</td>
+                <td>
+                  <div>
+                    <div>{area.name}</div>
+                    <div className="subtitle">{area.dh} Dehumidifier - {area.bl} Blower</div>
+                  </div>
+                </td>
+
+                <td>
+                  <div>{(totalLapsedTime - area.dryingTime).toFixed(2)}h Drying</div>
+                  <div className="subtitle">Pre-drying: {area.dryingTime}h - Total: {totalLapsedTime}h</div>
+                </td>
+
+                <td>
+                  <div>CAD${(area.totalEnergy * 0.17).toFixed(2)}</div>
+                  <div className="subtitle">{area.totalEnergy}kWh</div>
+                </td>
               </tr>
             </>
           ))}
@@ -61,11 +107,11 @@ function App({
       </table>
 
       <div className="footer">
-        <div className="rcn">Robbie Certificate Number: {robbieCertificateNumber}</div>
-        <div>www.robbiert.com</div>
-        <div className="incDetails">
-          <div>Robbie Restoration Technologies Inc.</div>
-          <div>1040 Martin Grove Rd Etobicoke, ON, Cananda.</div>
+        <div className="rcn">Robbie Certificate Number: {robbieCertificateNumber}<br></br>
+        <a href="https://robbier.com"> www.robbiert.com </a>
+        </div>
+        <div className="incDetails"> Robbie Restoration Technologies Inc. <br></br>
+        <span className="small">1040 Martin Grove Rd Etobicoke, ON, Cananda. </span>
         </div>
       </div>
     </div>
@@ -112,9 +158,9 @@ App.defaultProps = {
   address: '6-110 West Beaver Creek Road, Richmond Hill, ON, Canada',
   postalCode: 'L4B3L6',
   claimNumber: '5304B3981',
-  areas: [{name: "Men's washroom", dryingTime: 44.8, totalEnergy: 0.265, totalBlArea: 2, totalDhArea: 1},
-          {name: "Women's washroom", dryingTime: 48.45, totalEnergy: 0.707, totalBlArea: 2, totalDhArea: 1},
-          {name: "Hot water tank storage ar", dryingTime: 118.42, totalEnergy: 5.035, totalBlArea: 2, totalDhArea: 1}],
+  areas: [{ name: "Men's washroom", dryingTime: 44.8, totalEnergy: 0.265, totalBlArea: 2, totalDhArea: 1 },
+  { name: "Women's washroom", dryingTime: 48.45, totalEnergy: 0.707, totalBlArea: 2, totalDhArea: 1 },
+  { name: "Hot water tank storage ar", dryingTime: 118.42, totalEnergy: 5.035, totalBlArea: 2, totalDhArea: 1 }],
   totalAreas: '3',
   totalEnergyJob: '6.006',
   robbieCertificateNumber: '157368',
@@ -123,7 +169,7 @@ App.defaultProps = {
   totalCountBl: 6,
   totalCountDh: 3,
   demolitiontime: 0.0,
-  totaldryingtime: 118.60, 
+  totaldryingtime: 118.60,
 }
 
 
