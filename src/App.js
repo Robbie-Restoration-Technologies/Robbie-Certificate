fa1df4be6152cf22b06b97227925895073eb4cba
Ricardo Rambal <ricram1@gmail.com>
2023-02-25 12:19:29 -0500

    Adicion de las pausas hechas a las areas

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react' 
 import PropTypes from "prop-types";
 import './App.css';
 import { ReactComponent as RobbieLogo } from './Robbie.svg'
@@ -18,7 +19,8 @@ function App({
   totalCountDh,
   totalCountBl,
   demolitiontime,
-  totaldryingtime
+  totaldryingtime,
+  pauses,
 }) {
 
   return (
@@ -34,7 +36,6 @@ function App({
           <div className="subtitle"><span className="genDato">{postalCode}</span></div>
         </div>
       </div>
-
 
       <div className="generalData"></div>
       <div className="table-title blue-header">SUMMARY</div>
@@ -72,34 +73,39 @@ function App({
       <div className="generalData"></div>
       <div className="table-title blue-header">BREAKDOWN</div>
       <table width="100%">
-
         <thead align="left"><tr>
           <th className="setup">AREA SETUP</th>
           <th className="time">TIME BREAKDOWN</th>
           <th className="energy">ENERGY USED</th></tr>
         </thead>
-
         <tbody align="left">
           {areas.map((area, index) => (
             <>
-              <tr>
+              <tr key={index}>
                 <td>
                   <div>
                     <div>{area.name}</div>
                     <div className="subtitle">{area.dh} Dehumidifier - {area.blowers} Blower</div>
                   </div>
                 </td>
-
                 <td>
                   <div>{(area.drying_time).toFixed(2)}h Drying</div>
                   <div className="subtitle">Pre-drying: {(area.totalElapsedTimeA - area.drying_time).toFixed(2)}h - Total: {area.totalElapsedTimeA.toFixed(2)}h</div>
                 </td>
-
                 <td>
                   <div>CAD${(area.totenergy * area.drying_time * 0.17).toFixed(2)}</div>
                   <div className="subtitle">{(area.totenergy * area.drying_time).toFixed(2)}kWh</div>
                 </td>
               </tr>
+              {pauses.map((pause, index2) => (
+                pause.id === area.area ?
+                  <tr key={index2}>
+                    <td>{pause.created_at}</td>
+                    <td colSpan={2}>{pause.why}</td>
+                  </tr>
+                :
+                  null
+              ))}
             </>
           ))}
         </tbody>
@@ -107,7 +113,7 @@ function App({
 
       <div className="footer">
         <div className="rcn">Robbie Certificate Number: {robbieCertificateNumber}<br></br>
-        <a href="https://robbier.com"> www.robbiert.com </a>
+        <a href="https://robbiert.com"> www.robbiert.com </a>
         </div>
         <div className="incDetails"> Robbie Restoration Technologies Inc. <br></br>
         <span className="small">1040 Martin Grove Rd Etobicoke, ON, Cananda. </span>
@@ -132,47 +138,50 @@ App.propTypes = {
   totalCountBl: PropTypes.number,
   totalCountDh: PropTypes.number,
   demolitiontime: PropTypes.number,
-  totaldryingtime: PropTypes.number
+  totaldryingtime: PropTypes.number,
+  pauses: PropTypes.array,
 }
 
-
-App.defaultProps = {
-  headline: 'DRYING CERTIFICATE FOR:',
-  address: '',
-  postalCode: '',
-  claimNumber: '',
-  areas: [],
-  totalAreas: 0,
-  totalEnergyJob: 0.0,
-  robbieCertificateNumber: '',
-  totalEnergyCostJob: 0,
-  totalLapsedTime: 0,
-  totalCountBl: 0,
-  totalCountDh: 0,
-  demolitiontime: 0,
-  totaldryingtime: 0,
-  showLogos: true,
-}
-
-/* 
+// App.defaultProps = {
+//   headline: 'DRYING CERTIFICATE FOR:',
+//   address: '',
+//   postalCode: '',
+//   claimNumber: '',
+//   areas: [],
+//   totalAreas: 0,
+//   totalEnergyJob: 0.0,
+//   robbieCertificateNumber: '',
+//   totalEnergyCostJob: 0,
+//   totalLapsedTime: 0,
+//   totalCountBl: 0,
+//   totalCountDh: 0,
+//   demolitiontime: 0,
+//   totaldryingtime: 0,
+//   showLogos: true,
+//   pauses: [],
+// }
+ 
 App.defaultProps = {
   headline: 'DRYING CERTIFICATE FOR:',
   address: '6-110 West Beaver Creek Road, Richmond Hill, ON, Canada',
   postalCode: 'L4B3L6',
   claimNumber: '5304B3981',
-  areas: [{ name: "Men's washroom", drying_time: 44.8, totenergy: 0.265, blowers: 2, dh: 1, totalElapsedTimeA: 44.80 },
-  { name: "Women's washroom", drying_time: 48.45, totenergy: 0.707, blowers: 2, dh: 1, totalElapsedTimeA: 48.45 },
-  { name: "Hot water tank storage ar", drying_time: 118.42, totenergy: 5.035, blowers: 2, dh: 1, totalElapsedTimeA: 118.42 }],
+  areas: [{ area: 282, name: "Men's washroom", drying_time: 44.8, totenergy: 0.265, blowers: 2, dh: 1, totalElapsedTimeA: 44.80 },
+  { area: 283, name: "Women's washroom", drying_time: 48.45, totenergy: 0.707, blowers: 2, dh: 1, totalElapsedTimeA: 48.45 },
+  { area: 284, name: "Hot water tank storage ar", drying_time: 118.42, totenergy: 5.035, blowers: 2, dh: 1, totalElapsedTimeA: 118.42 }],
   totalAreas: '3',
-  totalEnergyJob: '6.006',
+  totalEnergyJob: 6.006,
   robbieCertificateNumber: '157368',
-  totalEnergyCostJob: '1.021',
+  totalEnergyCostJob: 1.021,
   totalLapsedTime: 118.6,
   totalCountBl: 6,
   totalCountDh: 3,
   demolitiontime: 0.0,
   totaldryingtime: 118.60,
+  pauses: [{id: 282, created_at: "2023-02-13 08:40:38", why: "Pause 1"},
+  {id: 282, created_at: "2023-02-13 13:56:09", why: "Pause 2"},
+  {id: 284, created_at: "2023-02-14 08:51:47", why: "Pause 3"}],
 }
-*/
+
 
 export default App;
